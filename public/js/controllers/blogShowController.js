66fee5da967a4de3870b02321abe3a43f19752da
Arthur <arthur.p.wright@gmail.com>
2015-07-22 00:11:47 -0400

    Added image to developer index

--- public/js/controllers/blogShowController.js
+++ public/js/controllers/blogShowController.js
@@ -1,4 +1,3 @@
 angular.module('ArthurWright').controller('BlogShowController', function(Blog, $scope, $routeParams){
   $scope.post = Blog.get({id: $routeParams.id});
-  console.log($scope.post);
 });
